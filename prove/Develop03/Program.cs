425fd34e30f81ad5373275fff0c2725f207d41d7
lauralizz <lauranrmn1@gmail.com>
2023-05-17 02:28:08 -0400

    W03 Prove Assignment completed

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,30 @@
 using System;
-
-class Program
+using System.Collections.Generic;
+public class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Develop03 World!");
+        ScriptureReference reference = new 
+        ScriptureReference("John", 3, 16);
+        Scripture scripture = new Scripture(reference, "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.");
+
+        Console.Clear();
+        Console.WriteLine(scripture.GetFormattedScripture());
+
+        while (scripture.HasHiddenWords())
+        {
+            Console.WriteLine("\nPress Enter to continue or type 'quit' to exit.");
+            string input = Console.ReadLine();
+
+            if (input == "quit")
+                break;
+
+            Console.Clear();
+            scripture.HideRandomWords();
+            Console.WriteLine(scripture.GetFormattedScripture());
+        }
+
+        Console.WriteLine("\nAll words have been hidden. Press Enter to exit.");
+        Console.ReadLine();
     }
 }
